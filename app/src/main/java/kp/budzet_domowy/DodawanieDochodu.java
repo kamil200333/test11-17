1085f6b6fc75f5381c83d2acfbc4d717fc9af406
Kamil Pawelec <kamil.m.pawelec@gmail.com>
2017-11-25 13:58:29 +0100

    Adding activity

---
+++ app/src/main/java/kp/budzet_domowy/DodawanieDochodu.java
@@ -0,0 +1,13 @@
+package kp.budzet_domowy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DodawanieDochodu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodawanie_dochodu);
+    }
+}
